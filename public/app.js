37ef2281fb5dabff967a546ef52ef248c30f71a2
ryland <ryland@MacBook-Pro-4.local>
2023-08-20 14:34:55 -0700

    added profile page styling and some functionality

--- public/app.js
+++ public/app.js
@@ -17,4 +17,39 @@
 //         })
 // }
 
+console.log('hi');
 
+if (document.querySelector('.roundProfPic')){
+    let profPic = document.querySelector('.roundProfPic')
+    profPic.addEventListener('click', (e)=>{
+        document.querySelector('body').innerHTML = `<div class="container"><img src=${profPic.src} class="profPic"> <br><a href="" class="link"><--Back</a></div>`
+        document.querySelector('body').style.backgroundColor = 'black'
+        document.querySelector('.profPic').style.height = '500px'
+        document.querySelector('.profPic').style.width = '500px'
+        document.querySelector('.container').style.margin = '200px'
+        document.querySelector('.container').style.height = '700px'
+        document.querySelector('.container').style.textAlign = 'center'
+        document.querySelector('.profPic').style.backgroundColor = 'white'
+        document.querySelector('body').style.display = 'flex'
+        document.querySelector('body').style.justifyContent = 'center'
+        document.querySelector('body').style.flexDirection = 'column'
+        document.querySelector('body').style.alignItems = 'center'
+        document.querySelector('.link').style.fontSize = '25px'
+    })
+}
+
+
+if (document.querySelector('.colorsNav')){
+    let colorsContainer = document.querySelector('.colorsContainer')
+    let colorsNav = document.querySelector('.colorsNav')
+    colorsNav.addEventListener('click', (e)=>{
+        if (colorsContainer.hidden==true){
+            colorsContainer.hidden = false
+            return
+        }
+        if (colorsContainer.hidden==false){
+            colorsContainer.hidden=true
+            return
+        }
+    })
+}
